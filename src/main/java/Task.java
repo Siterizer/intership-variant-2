b52e73a38649acc299b5314208b7b880aef31a43
siterizer <siterizer@gmail.com>
2020-03-06 21:10:16 +0100

    created 'returnDuplicatePostTitles' method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,7 +3,9 @@ import main.entites.user.User;
 import main.service.PostService;
 import main.service.UserService;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Task {
 
@@ -16,7 +18,7 @@ public class Task {
 
         List<User> users = UserService.loadUsers();
         List<Post> posts = PostService.loadPosts();
-        System.out.println(countUserPosts(users, posts));
+        System.out.println(returnDuplicatePostTitles(posts));
     }
     public static String countUserPosts(List<User> users, List<Post> posts){
         String result = "";
@@ -28,6 +30,19 @@ public class Task {
                 }
             }
             result = result.concat(user.getName() + "napisał(a)" + count + "postów" + "\n");
+        }
+        return result;
+    }
+    public static String returnDuplicatePostTitles(List<Post> posts){
+        Set<String> foundDuplicateTitles = new HashSet<String>();
+        String result = "";
+
+        for (Post post : posts) {
+            String title = post.getTitle();
+            if(foundDuplicateTitles.contains(title)){
+                result = result.concat(title + "\n");
+            }
+            foundDuplicateTitles.add(title);
         }
         return result;
     }
