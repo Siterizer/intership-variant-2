f4652a30b4d67100ef736c2ef9806e9a4f96fa28
siterizer <siterizer@gmail.com>
2020-03-06 19:52:09 +0100

    Added simple User Entites

---
+++ src/main/java/main/entites/Geo.java
@@ -0,0 +1,25 @@
+package main.entites;
+
+public class Geo {
+    private String lat;
+    private String lng;
+
+    // GETTERS & SETTERS //
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+    // GETTERS & SETTERS //
+}
