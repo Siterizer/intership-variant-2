167c7c35cbb0c6b753c187bd46683e0aadd1b924
siterizer <siterizer@gmail.com>
2020-03-08 19:45:37 +0100

    Deleted 'Main', library is ready

--- src/main/java/library/Main.java
+++
@@ -1,9 +0,0 @@
-package library;
-
-public class Main {
-    public static void main(String args[]){
-        UserAndPost userAndPost = new UserAndPost("https://jsonplaceholder.typicode.com/users", "https://jsonplaceholder.typicode.com/posts");
-        System.out.println(userAndPost.countUserPosts());
-
-    }
-}
